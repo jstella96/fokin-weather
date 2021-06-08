e78c1b784bf5ba99116e7c577ff36f7bf3399b1c
jstella96 <jstella0530@naver.com>
2021-06-08 09:04:51 +0900

    study: nomadcoder react-native -1.2

--- App.tsx
+++ App.tsx
@@ -1,41 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native"
+import Loading from "./Loading";
+import * as Location from 'expo-location'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Text hello</Text>
-      <Text style={styles.text2}>Text hello</Text>
-      <Text style={styles.text2}>Text hello</Text>
-      <Text style={styles.text3}>Text hello</Text>
-      <Text style={styles.text3}>Text hello</Text>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ff0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "#ffffff",
-    fontSize : 19
-    
-  },
-  text2: {
-    color: "#ffffff",
-    fontSize : 19
-    
-  },
-  text3: {
-    color: "#ffffff",
-    fontSize : 19
-    
+export default class extends React.Component {
+  getLocation = async() => {
+    try{
+      Alert.alert("실패")
+      const response = await Location.requestPermissionsAsync()
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync()
+      console.log(location)
+    }catch{
+      Alert.alert("실패")
+    }
   }
-});
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    return <Loading />
+  } 
+}
