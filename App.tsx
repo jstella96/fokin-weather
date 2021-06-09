61d8aa35bcd16165abb828494abadb8288836589
jstella96 <jstella0530@naver.com>
2021-06-09 09:03:03 +0900

    study: 1.4

--- App.tsx
+++ App.tsx
@@ -2,24 +2,37 @@ import React from 'react';
 import {Alert} from "react-native"
 import Loading from "./Loading";
 import * as Location from 'expo-location'
-
+import axios from "axios"
+const API_KEY = "49af4c5e49c5942d6b05123853461b81"
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getWeather = async (latitude,longitude) => {
+    const {data}  = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    )
+    console.log(data)
+  }
   getLocation = async() => {
     try{
-      Alert.alert("실패")
-      const response = await Location.requestPermissionsAsync()
-      console.log(response);
-      const location = await Location.getCurrentPositionAsync()
-      console.log(location)
+      await Location.requestPermissionsAsync()
+      const { 
+        coords : {latitude, longitude}
+      } = await Location.getCurrentPositionAsync()
+      //console.log(coords.latitude, coords.longitude)
+      this.getWeather(latitude,longitude)
+      //this.setState({isLoading:false})
     }catch{
-      Alert.alert("실패")
+      Alert.alert("can't find do","so sad")
     }
   }
   componentDidMount(){
     this.getLocation();
   }
   render(){
-    return <Loading />
+    const {isLoading}  = this.state;
+    return isLoading ? <Loading /> : null
   } 
 }
